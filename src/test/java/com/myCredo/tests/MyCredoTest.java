8e6a5ed7c8944533f563e7bafc0f249e30d2b5ab
BBSoftPro <BBSoftPro@gmail.com>
2024-07-28 16:36:45 +0400

    Config

---
+++ src/test/java/com/myCredo/tests/MyCredoTest.java
@@ -0,0 +1,40 @@
+package com.myCredo.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class MyCredoTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void openGoogle() {
+        driver.get("https://www.google.com");
+        System.out.println("Title: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test2() {
+        String randomAlphanumeric = RandomStringUtils.random(10, "abc123");
+        System.out.println("Random Alphanumeric String: " + randomAlphanumeric);
+    }
+
+}
