445841a23e8020d4a4dd82dba66690de321d8a01
Lasha Bibilashvili <BBSoftPro@gmail.com>
2024-07-29 20:24:36 +0400

    test utils

---
+++ src/test/java/com/myCredo/testsUtils/LoginTestUtils.java
@@ -0,0 +1,51 @@
+package com.myCredo.testsUtils;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.DataProvider;
+import org.testng.asserts.SoftAssert;
+import pages.LoginPage;
+import services.Bundle;
+import utils.StringUtils;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class LoginTestUtils extends TestUtils {
+
+    final List<String> dialects = Arrays.asList("ka", "meg", "svan");
+
+    @DataProvider(name = "loginData")
+    public Object[][] createLoginData() {
+        int count = dialects.size();
+        Object[][] data = new Object[count][count]; // Array to hold the test data
+
+        for (int i = 0; i < count; i++) {
+            int randomInt = new Random().nextInt((10 - 1) + 1) + 1;
+            String randomStr = RandomStringUtils.random(randomInt, StringUtils.enChars);
+            data[i][0] = randomStr;
+            data[i][1] = randomStr;
+            data[i][2] = dialects.get(i);
+        }
+
+        return data;
+    }
+
+    public void fillAuthFields(LoginPage page, String username, String password) {
+        page.job(it -> {
+            it.enterUsername(username);
+            it.enterPassword(password);
+        });
+    }
+
+    public static void checkWarningMessage(LoginPage l, String dialect, SoftAssert softAssert) {
+        String warningMessage = l.getWarningMessage();
+        try {
+            String str = new Bundle(dialect).getString("warning_message");
+            softAssert.assertEquals(warningMessage, str, warningMessage + " warning message is not found");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/myCredo/testsUtils/TestUtils.java
@@ -0,0 +1,7 @@
+package com.myCredo.testsUtils;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestUtils {
+    public static WebDriver driver;
+}
