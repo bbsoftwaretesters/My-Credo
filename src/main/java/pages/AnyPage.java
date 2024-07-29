e65e800d3e9288def050347f35e3dc0dc61eeb31
muggama <lashabibilashvili1983@gmail.com>
2024-07-29 06:07:49 +0400

    page models

---
+++ src/main/java/pages/AnyPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AnyPage extends BasePage<AnyPage> {
+    public AnyPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,67 @@
+package pages;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage<LoginPage> {
+
+    @FindBy(id = "userName")
+    private WebElement usernameField;
+
+    @FindBy(id = "newPass")
+    private WebElement passwordField;
+    @FindBy(id = "submitAuth")
+    private WebElement loginButton;
+
+    @FindBy(id = "growlText")
+    private WebElement warningMessage;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterUsername(@NonNull String username) {
+        waitFor(usernameField, 5);
+        usernameField.sendKeys(username);
+    }
+
+    public String getUsername() {
+        waitFor(usernameField, 5);
+        return usernameField.getText();
+    }
+
+    public void enterPassword(@NonNull String password) {
+        waitFor(passwordField, 5);
+        passwordField.sendKeys(password);
+    }
+
+    public String getWarningMessage() {
+        waitFor(warningMessage, 3);
+        return warningMessage.getText();
+    }
+
+    public HomePage login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        return clickLogin();
+    }
+
+    public HomePage clickLogin() {
+        loginButton.click();
+        return new HomePage(driver);
+    }
+
+    public <T extends BasePage<T>> T clickLogin(Class<T> klass) {
+        loginButton.click();
+        try {
+            return klass.getDeclaredConstructor(WebDriver.class).newInstance(driver);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create page instance", e);
+        }
+    }
+
+}
