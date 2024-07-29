b26d6a2bf1aa1200dddfd9071ca78d9992ad690c
muggama <lashabibilashvili1983@gmail.com>
2024-07-29 06:08:36 +0400

    test

--- src/test/java/com/myCredo/tests/MyCredoTest.java
+++ src/test/java/com/myCredo/tests/MyCredoTest.java
@@ -6,7 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.LoginPage;
+import utils.StringUtils;
+
+import java.util.Random;
 
 public class MyCredoTest {
 
@@ -18,10 +24,42 @@ public class MyCredoTest {
         driver = new ChromeDriver();
     }
 
-    @Test
-    public void openGoogle() {
-        driver.get("https://www.google.com");
-        System.out.println("Title: " + driver.getTitle());
+    @DataProvider(name = "loginData")
+    public Object[][] createLoginData() {
+        Object[][] data = new Object[10][2]; // Array to hold the test data
+
+        for (int i = 0; i < 10; i++) {
+            int randomInt = new Random().nextInt((10 - 1) + 1) + 1;
+            String randomStr = RandomStringUtils.random(randomInt, StringUtils.enChars);
+            data[i][0] = randomStr;
+            data[i][1] = randomStr;
+        }
+
+        return data;
+    }
+
+    @Test(dataProvider = "loginData", threadPoolSize = 5)
+    public void loginNegativeTest(String username, String password) {
+        SoftAssert softAssert = new SoftAssert();
+
+        // Initialize WebDriver inside the test method
+        driver.get("https://mycredo.ge/landing/main/auth");
+        System.out.println("Generated password: " + password);
+
+        new LoginPage(driver)
+                .job(l -> {
+                    l.enterUsername(username);
+                    System.out.println("entering Username: " + username);
+                    System.out.println("entered Username: " + l.getUsername());
+                    l.enterPassword(password);
+                })
+                .clickLogin(LoginPage.class)
+                .job(l -> {
+                    String warningMessage = l.getWarningMessage();
+                    softAssert.assertEquals(warningMessage, "მონაცემები არასწორია", warningMessage + " warning message is not found");
+                });
+
+        softAssert.assertAll();
     }
 
     @AfterClass
@@ -29,12 +67,6 @@ public class MyCredoTest {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    @Test
-    public void test2() {
-        String randomAlphanumeric = RandomStringUtils.random(10, "abc123");
-        System.out.println("Random Alphanumeric String: " + randomAlphanumeric);
     }
 
 }
